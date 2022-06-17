7789973620abcd831fba827a963ecaa20b8d2cfa
luishercules <fernando_hercules9@hotmail.com>
2022-06-17 10:41:56 -0600

    [UPDATE] Next Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*{html,js}'],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
     colors: {
       ...colors,
     },
   },
-  plugins: [require('@tailwindcss/forms')],
 };
